6c0c3d9f59b4d76146e54c275e4064e30cb7a808
GuilhermeDes <59022920+GuilhermeDes@users.noreply.github.com>
2021-02-19 19:05:21 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*Estudos*/
+function pomodoro() {
+var tempoEmMinutos = 1;
+var expiracao = new  Date(new Date().getTime() + tempoEmMinutos * 60000);
+
+console.log(expiracao);
+contador = window.setInterval(function(){
+    faltam = expiracao - new Date();
+    if (faltam <= 0){
+    	window.clearInerval(contador);
+    }
+    minutos = Math.floor(faltam / 60000);
+    segundos = faltam % 60000;
+    tempoRestante = `${minutos}:${segundos.toString().substring(0,2)}`;
+ document.getElementById('tempo').innerText = tempoRestante;
+    console.log(tempoRestante);
+}, 1000);
+}
+/*Intervalo*/
+/*Reset*/
